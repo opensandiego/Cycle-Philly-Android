882697a750302fb6161830ec7ed711c9053842df
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2013-09-14 13:45:18 -0400

    Move project to Eclipse; add library for Google Play services.

--- gen/edu/gatech/ppl/cycleatlanta/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package edu.gatech.ppl.cycleatlanta;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
